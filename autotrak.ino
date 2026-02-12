1a1314d974c66b6db3da0d8ca0cfc2618a458d4d
Grian Gajila <griangajila2@gmail.com>
2026-02-12 14:15:46 +0800

    wip

--- autotrak.ino
+++ autotrak.ino
@@ -2,15 +2,22 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <Firebase_ESP_Client.h>
-
+#include <Servo.h>
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
 
-#define LEFT_MOTOR D1
-#define RIGHT_MOTOR D2
+#define LEFT_ACTUATOR D1
+#define RIGHT_ACTUATOR D2 
 
-#define WIFI_SSID "DITO ANG WIFI NAME"
-#define WIFI_PASSWORD "DITO ANG WIFI PASSWORD"
+#define DOWN_ACTUATOR D3
+#define UP_ACTUATOR D4
+#define power D6
+
+Servo m1;
+Servo servo;
+
+#define WIFI_SSID "FERRYTALE 3782"
+#define WIFI_PASSWORD "mamamoblue"
 
 #define API_KEY "AIzaSyALdciRWibe5zTWarsi0yuzvMIvAkE7yww"
 #define DATABASE_URL "autotrak-31c44-default-rtdb.firebaseio.com/"
@@ -26,11 +33,17 @@ unsigned long sendDataPrevMillis = 0;
 
 void setup() {
   Serial.begin(115200);
-
-  pinMode(LEFT_MOTOR, OUTPUT);
-  pinMode(RIGHT_MOTOR, OUTPUT);
-  digitalWrite(LEFT_MOTOR, HIGH);
-  digitalWrite(RIGHT_MOTOR, HIGH);
+  servo.attach(D5);
+  pinMode(LEFT_ACTUATOR, OUTPUT);
+  pinMode(RIGHT_ACTUATOR, OUTPUT);
+  pinMode(DOWN_ACTUATOR, OUTPUT);
+  pinMode(UP_ACTUATOR, OUTPUT);
+  pinMode(power, OUTPUT);
+  digitalWrite(LEFT_ACTUATOR, HIGH);
+  digitalWrite(RIGHT_ACTUATOR, HIGH);
+  digitalWrite(DOWN_ACTUATOR, HIGH);
+  digitalWrite(UP_ACTUATOR, HIGH);
+  digitalWrite(power, HIGH);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
   while (WiFi.status() != WL_CONNECTED) {
@@ -67,24 +80,24 @@ void loop() {
 
     sendDataPrevMillis = millis();
 
-    if (Firebase.RTDB.getBool(&fbdo, "forward")) {
-      if (fbdo.dataType() == "boolean"){
-      bool forwardStateStr = fbdo.boolData();
-      Serial.println("Seccess: " + fbdo.dataPath() + ": " + forwardStateStr + "(" + fbdo.dataType() + ")");
-      bool forward = (forwardStateStr == false) ? HIGH : LOW;
-      digitalWrite(LEFT_MOTOR, forward);
-      digitalWrite(RIGHT_MOTOR, forward);
+    if (Firebase.RTDB.getInt(&fbdo, "forward")) {
+      if (fbdo.dataType() == "int"){
+        int powerValue = fbdo.intData();
+        powerValue = constrain(powerValue, 0, 180);
+        Serial.println("Seccess: " + fbdo.dataPath() + ":" + powerValue);
+        servo.write(powerValue);
       }
       
     } else {
       Serial.println("Failed to read Auto: " + fbdo.errorReason());
     }
+
     if (Firebase.RTDB.getBool(&fbdo, "left")) {
       if (fbdo.dataType() == "boolean"){
       bool leftStateStr = fbdo.boolData();
       Serial.println("Seccess: " + fbdo.dataPath() + ": " + leftStateStr + "(" + fbdo.dataType() + ")");
       bool left = (leftStateStr == false) ? HIGH : LOW;
-      digitalWrite(RIGHT_MOTOR, left);
+      digitalWrite(LEFT_ACTUATOR, left);
       }
       
     } else {
@@ -95,8 +108,42 @@ void loop() {
       bool rightStateStr = fbdo.boolData();
       Serial.println("Seccess: " + fbdo.dataPath() + ": " + rightStateStr + "(" + fbdo.dataType() + ")");
       bool right = (rightStateStr == false) ? HIGH : LOW;
-      digitalWrite(LEFT_MOTOR, right);
+      digitalWrite(RIGHT_ACTUATOR, right);
       }
+      
+    } else {
+      Serial.println("Failed to read Auto: " + fbdo.errorReason());
+    }
+
+    if (Firebase.RTDB.getBool(&fbdo, "suyodDown")) {
+      if (fbdo.dataType() == "boolean"){
+      bool downStateStr = fbdo.boolData();
+      Serial.println("Seccess: " + fbdo.dataPath() + ": " + downStateStr + "(" + fbdo.dataType() + ")");
+      bool down = (downStateStr == false) ? HIGH : LOW;
+      digitalWrite(DOWN_ACTUATOR, down);
+      }
+      
+    } else {
+      Serial.println("Failed to read Auto: " + fbdo.errorReason());
+    }
+    if (Firebase.RTDB.getBool(&fbdo, "suyodUp")) {
+      if (fbdo.dataType() == "boolean"){
+      bool upStateStr = fbdo.boolData();
+      Serial.println("Seccess: " + fbdo.dataPath() + ": " + upStateStr + "(" + fbdo.dataType() + ")");
+      bool up = (upStateStr == false) ? HIGH : LOW;
+      digitalWrite(UP_ACTUATOR, up);
+      }  
+      
+    } else {
+      Serial.println("Failed to read Auto: " + fbdo.errorReason());
+    }
+    if (Firebase.RTDB.getBool(&fbdo, "power")) {
+      if (fbdo.dataType() == "boolean"){
+      bool powerStateStr = fbdo.boolData();
+      Serial.println("Seccess: " + fbdo.dataPath() + ": " + powerStateStr + "(" + fbdo.dataType() + ")");
+      bool POWER = (powerStateStr == false) ? HIGH : LOW;
+      digitalWrite(power, POWER);
+      }  
       
     } else {
       Serial.println("Failed to read Auto: " + fbdo.errorReason());
